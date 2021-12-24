0a17e9dc2d3e75d11a402b62d81b3a62312bf15e
PlyshkaLim <beauty-gzhill@mail.ru>
2021-12-24 20:30:18 +0500

    Some

--- src/components/CalendarPage/CalendarDay/CalendarDay.jsx
+++ src/components/CalendarPage/CalendarDay/CalendarDay.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import styles from "./CalendarDay.module.css";
 import Calendar from "../../MainPage/Calendar/Calendar";
+import DemoApp from "../FullCalendar/FullCalendar";
 
 class CalendarDay extends React.Component {
     render() {
         return (<>
             <div className={styles.calendarDay}>
                 <div className={styles.calendarMap}>
-                    Понедельник, 14 июня
-                    <Calendar/>
+                    {/*Понедельник, 14 июня*/}
+                    {/*<Calendar/>*/}
+                    <DemoApp/>
                 </div>
             </div>
         </>)
